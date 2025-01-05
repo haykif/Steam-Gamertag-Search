cebeeabb7303dc8a20a7e18a4756b721cdaaed91
Hamza Akif Aydoğdu <94003315+haykif@users.noreply.github.com>
2025-01-05 16:01:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,6 @@ app.get('/getPlayerSummaries', async (req, res) => {
 app.get('/getOwnedGames', async (req, res) => {
     console.log('Requête reçue pour getOwnedGames avec params :', req.query);
     const { steamid } = req.query;
-    // const API_KEY = '5DA1986A3042A92A5D66781E4A853A48';
     const API_KEY = process.env.STEAM_API_KEY;
 
     if (!steamid) {
