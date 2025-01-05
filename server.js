5b5d9c11d8a9d31f7b50271b01b15bffe2c4709b
Hamza Akif Aydoğdu <h.a.akif@icloud.com>
2025-01-05 22:00:00 +0100

    mini maj

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 // set up rate limiter: maximum of 100 requests per 15 minutes
 const limiter = RateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // max 100 requests per windowMs
+    max: 100, // max 100 requetes par windowMs
 });
 
 // apply rate limiter to all requests
